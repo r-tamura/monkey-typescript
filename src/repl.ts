12ef6187e3f6375431e1decc7288ca80ac1edca9
r-tamura <rtamura30@gmail.com>
2018-08-25 00:00:40 +0900

    Add RLPL

--- src/repl.ts
+++ src/repl.ts
@@ -4,22 +4,33 @@
 import * as readline from "readline";
 import Lexer from "./lexer";
 import { Tokens } from "./token";
+import { removeListener } from "cluster";
 
 const PROMPT = ">> ";
-
+// https://nodejs.org/api/readline.html#readline_example_tiny_cli
 function start() {
+  const exit = () => {
+    console.log("\nREPL exit");
+    process.exit(0);
+  };
+
   readline
     .createInterface({
       input: process.stdin,
-      output: process.stdout
+      output: process.stdout,
+      prompt: PROMPT
     })
-    .question(PROMPT, input => {
-      const l = Lexer.of(input);
+    .on("line", function(line) {
+      const l = Lexer.of(line);
       let token;
       while ((token = l.nextToken()).type != Tokens.EOF) {
         console.log("%o", token);
       }
-    });
+      this.prompt();
+    })
+    .on("SIGINT", exit)
+    .on("close", exit)
+    .prompt();
 }
 
 export { start };
